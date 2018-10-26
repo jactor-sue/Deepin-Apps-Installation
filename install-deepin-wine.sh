d0621fd3563a75ae84efacb97faeb5b4f51aa161
Jactor-Sue <jactor_sue@live.com>
2018-10-26 22:21:16 +0800

    fix bug

--- install-deepin-wine.sh
+++ install-deepin-wine.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo "update cache"
+sudo apt update
+
 echo "installing libs..."
-sudo dpkg -i --force-overwrite ./libs/*
+sudo dpkg -i --force-overwrite ./libs/*.deb
 sudo apt  install -fy -o Dpkg::Options::="--force-overwrite"
 
 echo "installing deepin-wine..."
-sudo dpkg -i --force-overwrite ./deepin-wine/*
+sudo dpkg -i --force-overwrite ./deepin-wine/*.deb
 sudo apt  install -fy -o Dpkg::Options::="--force-overwrite"
 sudo apt autoremove
 
 echo "fixing notification bug..."
-sudo tar -zxvf ./bugfix/shell32.dll.so.tar.gz -C /usr/lib/i386-linux-gnu/deepin-wine/
+sudo tar -zxvf ./deepin-wine/bugfix/shell32.dll.so.tar.gz -C /usr/lib/i386-linux-gnu/deepin-wine/
 echo "done."
